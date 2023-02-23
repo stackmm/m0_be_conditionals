5e9c92bd23757ef3d1f095ce3bc33eafd812fb52
Steven Stackhouse <stackmm@gmail.com>
2023-02-23 11:51:31 -0700

    add mild_challenge.rb

---
+++ code_challenges/mild_challenge.rb
@@ -0,0 +1,10 @@
+# Write a Ruby program that defines a variable that stores an Integer. 
+# The program should print out the String "even" if the Integer is even, and the String "odd" if the Integer is odd.
+
+value = 4
+
+if value.odd?
+    puts "odd"
+else
+    puts "even"
+end
